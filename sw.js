055d3287bbc3685d0baea53b31c46c99e385cae2
Trastoo <loliko.pelaez@gmail.com>
2026-05-28 22:02:38 +0200

    Actualizar enlaces de icono y corregir rutas en el manifiesto y el Service Worker

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'turnos-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  '/dist/',
+  '/dist/index.html',
+  '/dist/manifest.json'
 ];
 
 self.addEventListener('install', event => {
