8b6d3373aea809c0038aa7d5006751178519e51d
Trastoo <loliko.pelaez@gmail.com>
2026-05-31 12:04:51 +0200

    Actualizar rutas de caché en el Service Worker para incluir iconos y mejorar la gestión de recursos

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 const CACHE_NAME = 'turnos-v1';
 const urlsToCache = [
-  '/dist/',
-  '/dist/index.html',
-  '/dist/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
