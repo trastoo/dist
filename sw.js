11339f7e4e35faa65c4adf8ef6fd08c5399d4713
Trastoo <loliko.pelaez@gmail.com>
2026-05-31 21:20:04 +0200

    Actualizar rutas de recursos en index.html, manifest.json y sw.js para mejorar la gestión de archivos

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
 const CACHE_NAME = 'turnos-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
