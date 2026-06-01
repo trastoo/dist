0db42c7c1f1f5a6a28387a229e099f4fb042af90
Trastoo <loliko.pelaez@gmail.com>
2026-06-01 15:26:20 +0200

    Refactor el service worker para mejorar la lógica de caché y la recuperación de recursos

--- sw.js
+++ sw.js
@@ -1,9 +1,23 @@
 const CACHE_NAME = 'quadrante-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
