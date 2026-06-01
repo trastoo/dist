8e3c73f3cdf82e358f579eb3dcd2a0ccae17cc43
Trastoo <loliko.pelaez@gmail.com>
2026-06-01 04:49:01 +0200

    Refactor code structure for improved readability and maintainability

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
-const CACHE_NAME = 'turnos-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
