6c3c7b5095510664fcc44e916b2e14289b8d130a
Trastoo <loliko.pelaez@gmail.com>
2026-06-01 16:35:17 +0200

    Refactor code structure for improved readability and maintainability

--- sw.js
+++ sw.js
@@ -1,23 +1,9 @@
 const CACHE_NAME = 'quadrante-v1';
-const urlsToCache = [
-  '/dist/',
-  '/dist/index.html',
-  '/dist/manifest.json',
-  '/dist/icon-192.png',
-  '/dist/icon-512.png'
-];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
